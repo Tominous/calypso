278f8c4ba9eaa792b70521d2eae3739056b89a9a
exception <erikrosem@gmail.com>
2017-04-04 20:43:39 -0300

    fix issue again.

--- src/shutdown.js
+++ src/shutdown.js
@@ -2,6 +2,9 @@ module.exports = {
     shutdown: function (client) {
         for (c in client.voiceChannels) {
             let channel = client.voiceChannels[c];
+            if (channel === undefined) {
+                continue;
+            }
             channel.leave();
         }
     }
