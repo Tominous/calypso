35ede2a7e3665cef624d85b35d4e79cc3889ac29
exception <erikrosem@gmail.com>
2017-05-15 20:43:54 -0300

    add help command

--- src/commands.js
+++ src/commands.js
@@ -68,7 +68,23 @@ let commands = [
         description: "Displays this message.",
         parameters: [],
         handle: function(message, params) {
-            message.author.sendMessage("Helperoni!");
+            let response = "**Available Commands:**\n";
+            response += "```";
+
+            for (let i = 0; i < commands.length; i++) {
+                let c = commands[i];
+                response += "\n~" + c.name;
+
+                for (let j = 0; j < c.parameters.length; j++) {
+                    response += " <" + c.parameters[j] + ">";
+                }
+
+                response += " : " + c.description;
+            }
+
+            response += "\n```";
+            message.author.sendMessage(response);
+            message.reply("Commands have been sent to your DMs");
         }
     }
 ];
