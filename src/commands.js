6b7ccca5f930b427849d0eae9986be71960fb8d1
exception <erikrosem@gmail.com>
2017-06-07 19:59:33 -0300

    hurdur

--- src/commands.js
+++ src/commands.js
@@ -197,9 +197,9 @@ let commands = [
         handle: function(message, params, client) {
             let embed = new Discord.RichEmbed();
             embed.setTitle(":pencil: Statistics").setColor("#259c28");
-            embed.addField("Uptime", "• Client: " + (process.uptime() + "").toHHMMSS() + "\n• Host: " + (require('os').uptime() + "").toHHMMSS(), true);
+            embed.addField("Uptime", "• Client: " + (process.uptime() + "").toHHMMSS() + "\n• Host: " + (require('os').uptime() + "").toHHMMSS(), false);
             embed.addField("General Stats", "• Guild Count: " + client.guilds.array().length + "\n• Users: " + client.users.array().length, true);
-            embed.addField("Other Data", "• Node Version: " + (process.version) + "\n• Discord.JS: v" + require('discord.js').version + "\n• Calypso: 1.2", true);
+            embed.addField("Other Data", "• Node Version: " + (process.version) + "\n• Discord.JS: v" + require('discord.js').version + "\n• Calypso: 1.2", false);
             embed.addField("Usage", "• Ram Usage: " +  (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + "MB", true);
             embed.setTimestamp();
             message.channel.sendEmbed(embed);
