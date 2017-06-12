85216e8ec14e96d59f8818e287d733e92b90e5fc
exception <erikrosem@gmail.com>
2017-06-12 18:25:57 -0300

    remove debug

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const {ShardingManager} = require('discord.js');
 const Manager = new ShardingManager('../src/calypso.js');
 
 Manager.spawn(2).then(col => {
-    console.log("Spawned " + col.array().length + " shards.")
 }).catch(err => console.log(err));
 
 process.on('unhandledRejection', err => console.error(err));
