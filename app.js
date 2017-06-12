1293f857be8df7537a1b079708452172ad0195c7
exception <erikrosem@gmail.com>
2017-06-12 18:07:13 -0300

    oh boy?

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// const {ShardingManager} = require('discord.js');
-// const Manager = new ShardingManager('../src/calypso.js');
-//
-// Manager.spawn(2).then(col => {
-//     console.log("Spawned " + col.array().length + " shards.")
-// }).catch(err => console.log(err));
-//
-// process.on('unhandledRejection', err => console.error(err));
+const {ShardingManager} = require('discord.js');
+const Manager = new ShardingManager('../src/calypso.js');
+
+Manager.spawn(2).then(col => {
+    console.log("Spawned " + col.array().length + " shards.")
+}).catch(err => console.log(err));
+
+process.on('unhandledRejection', err => console.error(err));
 
 module.exports = require("./src/calypso.js");
--- src/calypso.js
+++ src/calypso.js
@@ -33,7 +33,106 @@ client.on("ready", () => {
             client.voiceConnections = {};
             client.guildQueues = {};
 
-            console.log("Bot is currently in " + client.guilds.array().length + " guilds!");
+            console.log("Bot started on Shard #" + client.shard.id + "/" + client.shard.count + " with " + client.guilds.size + " guilds.");
+
+            client.on("message", message => {
+                if (!message.content.startsWith(starter)) {
+                    mongo.collection("received_messages").insertOne({
+                        "author": message.author.username,
+                        "authorId": message.author.id,
+                        "timetamp": new Date().getTime(),
+                        "message": message.content
+                    }, function(err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    });
+                    return;
+                }
+
+                if (toobusy()) {
+                    message.channel.send(message.author + " I'm currently under too much load. Please wait a couple of minutes. :dissapointed:");
+                    return;
+                }
+
+                let content = message.content.split(" ");
+                if (message.channel instanceof Discord.DMChannel) {
+                    mongo.collection("messages").insertOne({
+                        "author": message.author.username,
+                        "authorId": message.author.id,
+                        "timestamp": new Date().getTime(),
+                        "splitMessage": content,
+                        "message": message.content,
+                        "dm": true,
+                        "avatar": message.author.avatarURL
+                    }, function (err, result) {
+                        if (err !== null) {
+                            console.log(err);
+                        }
+                    });
+                } else {
+                    mongo.collection("messages").insertOne({
+                        "author": message.author.username,
+                        "authorId": message.author.id,
+                        "guild": message.guild.id,
+                        "timestamp": new Date().getTime(),
+                        "region": message.guild.region,
+                        "splitMessage": content,
+                        "message": message.content,
+                        "avatar": message.author.avatarURL
+                    }, function (err, result) {
+                        if (err !== null) {
+                            console.log(err);
+                        }
+                    });
+                }
+
+                cmdHandler.handleCommand(message, message.content.substring(1), client);
+            });
+
+            client.on("guildCreate", guild => {
+                mongo.collection("guilds").updateOne({
+                        "guildId": guild.id
+                    }, {
+                        "guildId": guild.id,
+                        "region": guild.region,
+                        "joinedAt": guild.joinedAt,
+                        "guildName": guild.name,
+                        "owner": guild.ownerId
+                    },
+                    {
+                        upsert: true
+                    }, function (err, object) {
+
+                    });
+                permissions.updateGuild(client, guild).then(object => {
+                }).catch(err => {
+                    console.log(err);
+                });
+            });
+
+            client.on("guildUpdate", (oldGuild, newGuild) => {
+                mongo.collection("guilds").updateOne({
+                        "guildId": oldGuild.id
+                    }, {
+                        "guildId": newGuild.id,
+                        "region": newGuild.region,
+                        "joinedAt": newGuild.joinedAt,
+                        "guildName": newGuild.name,
+                        "owner": newGuild.ownerId
+                    },
+                    {
+                        upsert: true
+                    }, function (err, object) {
+
+                    });
+
+                permissions.updateGuild(client, newGuild).then(object => {
+                    console.log("Updated guild")
+                }).catch(err => {
+                    console.log(err);
+                });
+            });
 
             for (let k in client.guilds.array()) {
                 let guild = client.guilds.array()[k];
@@ -60,105 +159,6 @@ client.on("ready", () => {
                 });
             }
         }
-    });
-});
-
-client.on("message", message => {
-    if (!message.content.startsWith(starter)) {
-        mongo.collection("received_messages").insertOne({
-            "author": message.author.username,
-            "authorId": message.author.id,
-            "timetamp": new Date().getTime(),
-            "message": message.content
-        }, function(err, result) {
-            if (err) {
-                console.log(err);
-            }
-        });
-        return;
-    }
-
-    if (toobusy()) {
-        message.channel.send(message.author + " I'm currently under too much load. Please wait a couple of minutes. :dissapointed:");
-        return;
-    }
-
-    let content = message.content.split(" ");
-    if (message.channel instanceof Discord.DMChannel) {
-        mongo.collection("messages").insertOne({
-            "author": message.author.username,
-            "authorId": message.author.id,
-            "timestamp": new Date().getTime(),
-            "splitMessage": content,
-            "message": message.content,
-            "dm": true,
-            "avatar": message.author.avatarURL
-        }, function (err, result) {
-            if (err !== null) {
-                console.log(err);
-            }
-        });
-    } else {
-        mongo.collection("messages").insertOne({
-            "author": message.author.username,
-            "authorId": message.author.id,
-            "guild": message.guild.id,
-            "timestamp": new Date().getTime(),
-            "region": message.guild.region,
-            "splitMessage": content,
-            "message": message.content,
-            "avatar": message.author.avatarURL
-        }, function (err, result) {
-            if (err !== null) {
-                console.log(err);
-            }
-        });
-    }
-
-    cmdHandler.handleCommand(message, message.content.substring(1), client);
-});
-
-client.on("guildCreate", guild => {
-    mongo.collection("guilds").updateOne({
-            "guildId": guild.id
-        }, {
-            "guildId": guild.id,
-            "region": guild.region,
-            "joinedAt": guild.joinedAt,
-            "guildName": guild.name,
-            "owner": guild.ownerId
-        },
-        {
-            upsert: true
-        }, function (err, object) {
-
-        });
-    permissions.updateGuild(client, guild).then(object => {
-    }).catch(err => {
-        console.log(err);
-    });
-});
-
-client.on("guildUpdate", (oldGuild, newGuild) => {
-    mongo.collection("guilds").updateOne({
-            "guildId": oldGuild.id
-        }, {
-            "guildId": newGuild.id,
-            "region": newGuild.region,
-            "joinedAt": newGuild.joinedAt,
-            "guildName": newGuild.name,
-            "owner": newGuild.ownerId
-        },
-        {
-            upsert: true
-        }, function (err, object) {
-
-        });
-
-    permissions.updateGuild(client, newGuild).then(object => {
-        console.log("Updated guild")
-    }).catch(err => {
-        console.log(err);
     });
 });
 
