adad4194c924606a00c973f4de99466692a90ac7
exception <erikrosem@gmail.com>
2017-06-12 17:57:32 -0300

    Debug shard count.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const {ShardingManager} = require('discord.js');
 const Manager = new ShardingManager('../src/calypso.js');
 
 Manager.spawn(2).then(col => {
-    console.log(col)
+    console.log("Spawned " + col.array().length + " shards.")
 }).catch(err => console.log(err));
 
 process.on('unhandledRejection', err => console.error(err));
