6a158a951bc337bd40dca274abef7c0699564da8
exception <erikrosem@gmail.com>
2017-06-12 17:51:31 -0300

    Sharding start?

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const {ShardingManager} = require('discord.js');
+const Manager = new ShardingManager('./src/calypso.js');
+
+Manager.spawn(2).then(col => {
+    console.log(col)
+}).catch(err => console.log(err));
+
+process.on('unhandledRejection', err => console.error(err));
+
 module.exports = require("./src/calypso.js");
