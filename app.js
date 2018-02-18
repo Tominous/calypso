c50d81acbb25480f6b7d006c37e986d50d007dc9
exception <erikrosem@gmail.com>
2018-02-18 20:43:24 -0300

    reree

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const {ShardingManager} = require('discord.js');
 const Manager = new ShardingManager('../src/calypso.js');
 
-Manager.spawn(2).then(col => {
+Manager.spawn(1).then(col => {
 }).catch(err => console.log(err));
 
 process.on('unhandledRejection', err => console.error(err));
