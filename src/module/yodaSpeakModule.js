5cea6aa008a3057938cb75f2d8306c7a6d494c24
exception <erikrosem@gmail.com>
2018-02-19 17:07:25 -0300

    bad idea

--- src/module/yodaSpeakModule.js
+++ src/module/yodaSpeakModule.js
@@ -8,13 +8,5 @@ module.exports = {
 
     },
     onChat: function(message, client) {
-        if (message.author.bot || message.guild.id === "310104725078933525") {
-            return
-        }
-        const guild = client.guilds.get("310104725078933525")
-        if (guild) {
-            const channel = guild.channels.get("415205740765118474")
-            channel.send(message.author + " (" + message.guild.name + " #" + message.channel.name + ") => " + message.content)
-        }
     }
 }
