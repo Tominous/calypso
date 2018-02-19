f00708dd16d969f1e04445bb37cf0f490cd9ff68
exception <erikrosem@gmail.com>
2018-02-18 21:40:17 -0300

    dev

--- src/commands/fun/yodaSpeak.js
+++ src/commands/fun/yodaSpeak.js
@@ -1,36 +1,12 @@
 const YodaSpeak = require('yoda-speak'),
     yoda = new YodaSpeak("QGOXQtQCgkmshQZ216bcDj8RaRX4p1yguhIjsn88GLh3e3Iybw")
 
-let state = false // enabled
-let client = undefined
-
-let prepareClient = (newClient) => {
-    this.client = newClient
-    newClient.on("message", message => {
-        if (state) {
-            message.delete().then(() => {
-                let response = yoda.convert(message.content)
-                message.channel.send(message.author + " " + response)
-            }).catch(() => {
-                message.reply("To talk like yoda failed!")
-            })
-        }
-    })
-}
-
 module.exports = {
     name: "yodaSpeak",
-    description: "Turns chat into Yoda mode.",
+    description: "Turns chat into Yoda Speak.",
     handle: function(message, params, client) {
-        if (state) {
-            message.reply("The chat is no longer yoda :(")
-            state = false
-        } else {
-            message.reply("The chat is now yoda mode!!!!!!")
-
-            prepareClient(client)
-
-            state = true
-        }
+        params.shift()
+        let text = params.join(" ")
+        message.reply("`" + yoda.convert(text) + "`")
     }
 }
