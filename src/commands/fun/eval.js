9f97f379a55b0e6f1fcc0f396a188fa8698d4b43
exception <erikrosem@gmail.com>
2018-02-19 14:19:24 -0300

    dev

--- src/commands/fun/eval.js
+++ src/commands/fun/eval.js
@@ -2,27 +2,21 @@ module.exports = {
     name: "eval",
     description: "Evaluate JavaScript and run it",
     parameters: ["script"],
+    ownerOnly: true,
     handle: function(message, params, client) {
-        permissions.isGlobalOwner(message.author).then(res => {
-            if (!res) {
-                message.reply(":shield: No permissions. Only bot owners can execute this command.");
-                logger.logPermissionFailed(message, message.author, client.mongo);
-            } else {
-                params.shift()
-                let script = params.join(" ")
-                message.reply(":satellite_orbital: Evaluating `" + script + "`").then((reply) => {
-                    try {
-                        let ran = eval(script)
-                        if (ran) {
-                            reply.edit(":white_check_mark: Finished evaluating. Response: `" + ran + "`")
-                        } else {
-                            reply.edit(":white_check_mark: Done evaluating. No data was returned.")
-                        }
-                    } catch (e) {
-                        reply.edit("Failed to evaluate. `" + e.message + "`")
-                        console.error(e)
-                    }
-                })
+        params.shift()
+        let script = params.join(" ")
+        message.reply(":satellite_orbital: Evaluating `" + script + "`").then((reply) => {
+            try {
+                let ran = eval(script)
+                if (ran) {
+                    reply.edit(":white_check_mark: Finished evaluating. Response: `" + ran + "`")
+                } else {
+                    reply.edit(":white_check_mark: Done evaluating. No data was returned.")
+                }
+            } catch (e) {
+                reply.edit("Failed to evaluate. `" + e.message + "`")
+                console.error(e)
             }
         })
     }
