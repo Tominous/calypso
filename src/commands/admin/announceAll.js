38ff9b1cf4d890519e97807409f5cca827021fde
exception <erikrosem@gmail.com>
2018-02-18 21:55:37 -0300

    start ownerOnly

--- src/commands/admin/announceAll.js
+++ src/commands/admin/announceAll.js
@@ -3,7 +3,25 @@ module.exports = {
     description: "Announces to all servers",
     params: ["text"],
     ownerOnly: true,
-    handle: function(message, params, client) {
-        message.reply("REEE")
+    handle: async function(message, params, client) {
+        message.reply("Sending announcement...").then(reply => {
+            params.shift()
+            let text = params.join(" ")
+            const guilds = client.guilds.array()
+
+            let allPromises = []
+
+            for (let i = 0; i < guilds.length; i++) {
+                let guild = guilds[i]
+                let channel = guild.defaultChannel
+                if (channel) {
+                    let sendMessagePromise = channel.sendMessage(":rocket: Announcement! " + text)
+                    allPromises.push(sendMessagePromise)
+                }
+            }
+
+            await Promise.all(allPromises)
+            reply.edit("Finished announcement. Sent to " + allPromises.length + " guilds.")
+        })
     }
 }
